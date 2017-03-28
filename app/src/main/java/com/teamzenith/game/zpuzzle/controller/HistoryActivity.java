ef95368166ce279d2ace9c1cf8138e7ab5d59caf
Alaa AlKassar <alaaalkassar@n135-p120.kthopen.kth.se>
2017-03-28 13:28:09 +0200

    Update login

---
+++ app/src/main/java/com/teamzenith/game/zpuzzle/controller/HistoryActivity.java
@@ -0,0 +1,35 @@
+package com.teamzenith.game.zpuzzle.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.teamzenith.game.zpuzzle.R;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.history_toolbar);
+        toolbar.setTitle("History");
+        setSupportActionBar(toolbar);
+        // add back arrow to toolbar
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
