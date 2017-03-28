480e12ba6caa4c73e223502f503e1c275986c852
hichem <hichem.memmi@outlook.com>
2017-03-28 10:16:06 +0200

    rename the class

---
+++ app/src/main/java/com/teamzenith/game/zpuzzle/model/Easy.java
@@ -0,0 +1,23 @@
+package com.teamzenith.game.zpuzzle.model;
+
+/**
+ * Created by memmi on 2017-03-09.
+ */
+
+public class Easy extends Level {
+
+    public static final int ROW = 3;
+    public static final int COLUMN = 3;
+
+    /**
+     * @return
+     */
+    public String toString() {
+        return "kids";
+    }
+
+    @Override
+    public void test() {
+
+    }
+}
