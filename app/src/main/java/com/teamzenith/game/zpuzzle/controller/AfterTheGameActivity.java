535c4ecb92ef227989560888fc09bdc59b799b4c
hichem <hichem.memmi@outlook.com>
2017-03-28 16:29:41 +0200

    Add textView to get the movement

--- app/src/main/java/com/teamzenith/game/zpuzzle/controller/AfterTheGameActivity.java
+++ app/src/main/java/com/teamzenith/game/zpuzzle/controller/AfterTheGameActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.teamzenith.game.zpuzzle.R;
 import com.teamzenith.game.zpuzzle.model.Level;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by Hichem Memmi on 2017-03-27.
@@ -18,12 +21,17 @@ public class AfterTheGameActivity extends AppCompatActivity implements View.OnCl
     private Button playAgainBtn;
     private Button goToMainBtn;
     private Level level;
+    private  TextView movementTextView;
+    private Intent intentFromGameActivity;
 
     public void onCreate(Bundle bundle){
         super.onCreate(bundle);
         setContentView(R.layout.after_the_game);
-        Intent intentFromGameActivity=getIntent();
+        intentFromGameActivity=getIntent();
        level=(Level) intentFromGameActivity.getSerializableExtra("Level");
+
+
+
         createComponents();
         actions();
 
@@ -33,12 +41,16 @@ public class AfterTheGameActivity extends AppCompatActivity implements View.OnCl
     private void createComponents() {
         playAgainBtn=(Button) findViewById(R.id.playAgain);
         goToMainBtn=(Button) findViewById(R.id.backToMain);
+        movementTextView=(TextView) findViewById(R.id.movementCounter);
+
     }
 
 
     private void actions() {
         playAgainBtn.setOnClickListener(this);
         goToMainBtn.setOnClickListener(this);
+        String countMovementString=intentFromGameActivity.getStringExtra("CountMovement");
+        movementTextView.setText("You solved on "+ countMovementString);
     }
 
     @Override
