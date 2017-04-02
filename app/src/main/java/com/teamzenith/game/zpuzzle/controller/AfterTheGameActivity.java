852dbe993d70920ca09befe809a2e663281beda9
hichem <hichem.memmi@outlook.com>
2017-04-02 04:19:03 +0200

    improve the activity

--- app/src/main/java/com/teamzenith/game/zpuzzle/controller/AfterTheGameActivity.java
+++ app/src/main/java/com/teamzenith/game/zpuzzle/controller/AfterTheGameActivity.java
@@ -9,12 +9,16 @@ import android.text.Html;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.teamzenith.game.zpuzzle.R;
 import com.teamzenith.game.zpuzzle.model.Level;
+import com.teamzenith.game.zpuzzle.model.User;
+import com.teamzenith.game.zpuzzle.model.UserHistoryEntry;
 
 
 import java.io.File;
+import java.text.ParseException;
 
 /**
  * Created by Hichem Memmi on 2017-03-27.
@@ -24,6 +28,7 @@ public class AfterTheGameActivity extends AppCompatActivity implements View.OnCl
 
     private ImageView playAgainBtn;
     private ImageView goToMainBtn;
+    private ImageView saveHistory;
     private Level level;
     private  TextView movementTextView;
     private Intent intentFromGameActivity;
@@ -31,12 +36,24 @@ public class AfterTheGameActivity extends AppCompatActivity implements View.OnCl
     private ImageView solvedImage;
     private float scale;
 
+    private HistoryController historyController;
+    private User user;
+    private String userID;
+    private String timerCounterString;
+    private String countMovementString;
+    private UserHistoryEntry userHistoryEntry;
+    private User player;
+    private ImageChooser.Method method;
+
+
     public void onCreate(Bundle bundle){
         super.onCreate(bundle);
         setContentView(R.layout.after_the_game);
         intentFromGameActivity=getIntent();
        level=(Level) intentFromGameActivity.getSerializableExtra("Level");
         scale = getApplicationContext().getResources().getDisplayMetrics().density;
+        historyController = new HistoryController();
+
         createComponents();
         actions();
     }
@@ -44,39 +61,70 @@ public class AfterTheGameActivity extends AppCompatActivity implements View.OnCl
     private void createComponents() {
         playAgainBtn=(ImageView) findViewById(R.id.playAgain);
         goToMainBtn=(ImageView) findViewById(R.id.backToMain);
+        saveHistory = (ImageView) findViewById(R.id.save_history);
         movementTextView=(TextView) findViewById(R.id.movementCounter);
         timerTextView=(TextView) findViewById(R.id.timer);
         solvedImage=(ImageView) findViewById(R.id.solvedImage);
+        player = (User) intentFromGameActivity.getSerializableExtra("player");
+        method=(ImageChooser.Method)intentFromGameActivity.getSerializableExtra("method");
     }
 
 
     private void actions() {
         playAgainBtn.setOnClickListener(this);
         goToMainBtn.setOnClickListener(this);
-        String countMovementString=intentFromGameActivity.getStringExtra("CountMovement");
+        saveHistory.setOnClickListener(this);
 
+        countMovementString=intentFromGameActivity.getStringExtra("CountMovement");
         String countMovementStringColored = "<font color='#EE0000'>"+countMovementString+"</font>";
         movementTextView.setText(Html.fromHtml("You solved on "+ countMovementStringColored+" steps"));
 
-        String timerCounterString=intentFromGameActivity.getStringExtra("TimerCounter");
+        timerCounterString=intentFromGameActivity.getStringExtra("TimerCounter");
         String timerCounterStringColored = "<font color='#EE0000'>"+timerCounterString+"</font>";
         timerTextView.setText(Html.fromHtml("Your time was "+timerCounterStringColored+" seconds"));
 
 
-        File imageFile = (File)intentFromGameActivity.getSerializableExtra("Image");
-        Bitmap solved = Bitmap.createScaledBitmap(BitmapFactory.decodeFile(imageFile.getAbsolutePath()), (int)(300 * scale), (int)(300 * scale), true);
+
+        Bitmap solved=null;
+
+        if(method.equals(ImageChooser.Method.RANDOM)){
+            int idOfDrawable=intentFromGameActivity.getIntExtra("Image",0);
+            solved = BitmapFactory.decodeResource(getResources(), idOfDrawable);
+            solved =Bitmap.createScaledBitmap(solved, (int) (300 * scale), (int) (300 * scale), false);
+        }
+
+        else{
+            File imageFile = (File)intentFromGameActivity.getSerializableExtra("Image");
+            solved = Bitmap.createScaledBitmap(BitmapFactory.decodeFile(imageFile.getAbsolutePath()), (int)(300 * scale), (int)(300 * scale), true);
+        }
+
         solvedImage.setImageBitmap(solved);
+
+
+        userID=player.getUserID();
+        userHistoryEntry = new UserHistoryEntry(userID,level,countMovementString,timerCounterString);
     }
 
     @Override
     public void onClick(View v) {
         if(v.getId()==R.id.playAgain){
-            Intent playAgainIntent=new Intent(getBaseContext(),Game.class);
+            Intent playAgainIntent=new Intent(getBaseContext(),ImageChooser.class);
             playAgainIntent.putExtra("Level",level);
+            playAgainIntent.putExtra("player",player);
             startActivity(playAgainIntent);
+        }
+        else if (v.getId() == R.id.save_history) {
+            Toast.makeText(this, "History Saved", Toast.LENGTH_SHORT).show();
+
+            try {
+                historyController.save(userHistoryEntry);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
         }
         else{
             Intent goBackToMain=new Intent(getBaseContext(),MainActivity.class);
+            goBackToMain.putExtra("player",player);
             startActivity(goBackToMain);
         }
     }
